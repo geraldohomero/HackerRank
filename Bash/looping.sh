e15480cba7919de9f0b55a5b8e851b54ccf486f1
Geraldo Homero <geraldohomero@protonmail.com>
2023-07-20 16:32:55 +0000

    Add HackerRank

---
+++ Bash/looping.sh
@@ -0,0 +1,6 @@
+num=1
+for i in {1..50}
+do
+  echo $num
+  num=$((num+1))
+done
---
+++ Bash/theWorldofNumbers.sh
@@ -0,0 +1,12 @@
+read a
+read b
+
+soma=$((a + b))
+diferenca=$((a - b))
+produto=$((a * b))
+quociente=$((a / b))
+
+echo $soma
+echo $diferenca
+echo $produto
+echo $quociente
